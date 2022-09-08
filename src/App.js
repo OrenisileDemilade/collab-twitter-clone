f7ffecc6c6f4853620bd54e181c96398993a1801
Design Synchrony <103902479+designsynchrony@users.noreply.github.com>
2022-09-08 11:50:44 -0700

    some changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import './App.css';
 import Sidebar from './components/Sidebar';
 import Middle from './components/Middle';
 import Aside from './components/Aside';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
-    <div className='HomePage'>
-      <Sidebar/>
-      <Middle/>
-      <Aside/>
-  </div>
+    <div className='main'>
+      <div className='HomePage'>
+        <Sidebar />
+        <div className='other'>
+          <Middle/>
+          <Aside />
+        </div>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
--- src/components/SidebarLink.js
+++ src/components/SidebarLink.js
@@ -2,11 +2,11 @@ import React from 'react'
 import './SidebarLink.css'
 import {Tooltip} from '@mui/material'
 
-function SidebarLink({Icon,text}) {
+function SidebarLink({Icon,text, fontSize}) {
   return (
     <Tooltip title={text} enterDelay={300} style={{fontSize:'3rem'}}>
         <div className='sidebar-links'>
-          <Icon className='sidebar-icon'/>
+        <Icon className='sidebar-icon' style={{fontSize: `${fontSize ? fontSize : '3rem'}`}} />
         </div>
     </Tooltip>
   )
