c497fe7dc0fdf4c87114a3ce00f2e31a65996aa2
OrenisileDemilade <dorenisile@gmail.com>
2022-09-14 00:09:12 +0100

    did post functionality

--- src/components/Input.js
+++ src/components/Input.js
@@ -8,15 +8,6 @@ import LeaderboardOutlinedIcon from '@mui/icons-material/LeaderboardOutlined';
 import SentimentSatisfiedAltOutlinedIcon from '@mui/icons-material/SentimentSatisfiedAltOutlined';
 import TodayOutlinedIcon from '@mui/icons-material/TodayOutlined';
 import Picker from '@emoji-mart/react'
-import { db, storage } from "../firebase";
-import {
-  addDoc,
-  collection,
-  doc,
-  serverTimestamp,
-  updateDoc,
-} from "@firebase/firestore";
-import { getDownloadURL, ref, uploadString } from "@firebase/storage";
 
 
 
@@ -52,31 +43,6 @@ function Input() {
   const sendPost = async () => {
     if (loading) return;
     setLoading(true);
-    
-    const docRef = await addDoc(collection(db,'posts'), {
-      // id: session.user.uid,
-      // username: session.user.name,
-      // userImg: session.user.image,
-      // tag: session.user.tag,
-      text: input,
-      timestamp: serverTimestamp(),
-    });
-
-    const imageRef = ref(storage, `posts/${docRef.id}/image`);
-
-    if (selectedFile){
-      await uploadString(imageRef, selectedFile, "data_url").then(async () => {
-        const downloadURL = await getDownloadURL (imageRef)
-        await updateDoc(doc(db, "posts", docRef.id),{
-          image: downloadURL,
-        });
-      });
-    }
-
-    setLoading(false);
-    setInput("");
-    setSelectedFile(null);
-    setShowEmojis(false);
   };
 
 
--- src/components/Middle.js
+++ src/components/Middle.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './Middle.css'
 import AutoAwesomeOutlinedIcon from '@mui/icons-material/AutoAwesomeOutlined';
+import VerifiedIcon from '@mui/icons-material/Verified';
 import Input from './Input';
+import Post from './Post';
 
 
 function middle() {
@@ -18,9 +20,25 @@ function middle() {
 
       <Input/>
 
-        <div className='post-container'>
-          
-        </div>
+      <Post
+      avatar="https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074__480.jpg"
+      verified={<VerifiedIcon/>}
+      displayName="Boogie"
+      username="@boogieman"
+      text="So the sacked dancing security men went abroad with their uniforms & continue dancing. 
+      I love Nigerians "
+      image="https://cdn.pixabay.com/photo/2015/02/04/17/18/space-624054__480.jpg"
+      />
+       <Post
+      avatar="https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074__480.jpg"
+      verified={<VerifiedIcon/>}
+      displayName="Boogie"
+      username="@boogieman"
+      text="So the sacked dancing security men went abroad with their uniforms & continue dancing. 
+      I love Nigerians "
+      image="https://cdn.pixabay.com/photo/2015/02/04/17/18/space-624054__480.jpg"
+      />
+      
     </div>
   )
 }
